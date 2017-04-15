507d87183b7428f9b45c99cc664244dd41129294
orestisf <orestisf1993@gmail.com>
2017-04-15 20:58:15 +0300

    Reformat python files of interest

--- art_autonomous_exploration/src/laser_data_aggregator.py
+++ art_autonomous_exploration/src/laser_data_aggregator.py
@@ -3,22 +3,20 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 
-# Class for reading the data from the laser sensor
-class LaserDataAggregator:
 
+# Class for reading the data from the laser sensor
+class LaserDataAggregator(object):
     # Constructor
     def __init__(self):
-
-        # Initialization of laser scan 
+        # Initialization of laser scan
         self.laser_scan = []
 
         # ROS Subscribers to the robot's laser
         laser_topic = rospy.get_param("laser_topic")
-        rospy.Subscriber(laser_topic, LaserScan, self.getDataLaser) 
+        rospy.Subscriber(laser_topic, LaserScan, self.getDataLaser)
 
     # Getting data from the laser
     def getDataLaser(self, data):
-
         # Get the measurements
         self.laser_scan = list(data.ranges)
 
@@ -28,4 +26,3 @@ class LaserDataAggregator:
                 self.laser_scan[i] = data.range_max
             elif self.laser_scan[i] < data.range_min:
                 self.laser_scan[i] = data.range_min
-
--- art_autonomous_exploration/src/target_selection.py
+++ art_autonomous_exploration/src/target_selection.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python
 
-import rospy
 import random
-import math
 import time
-import numpy as np
-from timeit import default_timer as timer
-from utilities import RvizHandler
+
+from brushfires import Brushfires
+from path_planning import PathPlanning
+from topology import Topology
 from utilities import OgmOperations
 from utilities import Print
-from brushfires import Brushfires
-from topology import Topology
-import scipy
-from path_planning import PathPlanning
+from utilities import RvizHandler
 
 
 # Class for selecting the next best target
-class TargetSelection:
-    
+class TargetSelection(object):
     # Constructor
     def __init__(self, selection_method):
         self.goals_position = []
@@ -30,14 +25,11 @@ class TargetSelection:
         self.topo = Topology()
         self.path_planning = PathPlanning()
 
-
-    def selectTarget(self, init_ogm, coverage, robot_pose, origin, \
-        resolution, force_random = False):
-        
+    def selectTarget(self, init_ogm, coverage, robot_pose, origin, resolution, force_random=False):
         target = [-1, -1]
 
         ######################### NOTE: QUESTION  ##############################
-        # Implement a smart way to select the next target. You have the 
+        # Implement a smart way to select the next target. You have the
         # following tools: ogm_limits, Brushfire field, OGM skeleton,
         # topological nodes.
 
@@ -55,53 +47,46 @@ class TargetSelection:
 
         # Calculate skeletonization
         tinit = time.time()
-        skeleton = self.topo.skeletonizationCffi(ogm, \
-                   origin, resolution, ogm_limits)
+        skeleton = self.topo.skeletonizationCffi(ogm,
+                                                 origin, resolution, ogm_limits)
         Print.art_print("Skeletonization time: " + str(time.time() - tinit), Print.ORANGE)
 
         # Find topological graph
         tinit = time.time()
-        nodes = self.topo.topologicalNodes(ogm, skeleton, coverage, origin, \
-                resolution, brush, ogm_limits)
+        nodes = self.topo.topologicalNodes(ogm, skeleton, coverage, origin,
+                                           resolution, brush, ogm_limits)
         Print.art_print("Topo nodes time: " + str(time.time() - tinit), Print.ORANGE)
-        
+
         # Visualization of topological nodes
         vis_nodes = []
         for n in nodes:
-            vis_nodes.append([
-                n[0] * resolution + origin['x'],
-                n[1] * resolution + origin['y']
-            ])
-        RvizHandler.printMarker(\
-            vis_nodes,\
-            1, # Type: Arrow
-            0, # Action: Add
-            "map", # Frame
-            "art_topological_nodes", # Namespace
-            [0.3, 0.4, 0.7, 0.5], # Color RGBA
-            0.1 # Scale
+            vis_nodes.append([n[0] * resolution + origin['x'], n[1] * resolution + origin['y']])
+        RvizHandler.printMarker(
+            vis_nodes,
+            1,  # Type: Arrow
+            0,  # Action: Add
+            "map",  # Frame
+            "art_topological_nodes",  # Namespace
+            [0.3, 0.4, 0.7, 0.5],  # Color RGBA
+            0.1  # Scale
         )
-    
+
         # Random point
         if self.method == 'random' or force_random == True:
-          target = self.selectRandomTarget(ogm, coverage, brush, ogm_limits)
+            target = self.selectRandomTarget(ogm, coverage, brush, ogm_limits)
         ########################################################################
-
         return target
 
     def selectRandomTarget(self, ogm, coverage, brushogm, ogm_limits):
-      # The next target in pixels
+        # The next target in pixels
         tinit = time.time()
-        next_target = [0, 0] 
+        next_target = [0, 0]
         found = False
         while not found:
-          x_rand = random.randint(0, ogm.shape[0] - 1)
-          y_rand = random.randint(0, ogm.shape[1] - 1)
-          if ogm[x_rand][y_rand] < 50 and coverage[x_rand][y_rand] < 50 and \
-              brushogm[x_rand][y_rand] > 5:
-            next_target = [x_rand, y_rand]
-            found = True
-        Print.art_print("Select random target time: " + str(time.time() - tinit), \
-            Print.ORANGE)
+            x_rand = random.randint(0, ogm.shape[0] - 1)
+            y_rand = random.randint(0, ogm.shape[1] - 1)
+            if ogm[x_rand][y_rand] < 50 and coverage[x_rand][y_rand] < 50 and brushogm[x_rand][y_rand] > 5:
+                next_target = [x_rand, y_rand]
+                found = True
+        Print.art_print("Select random target time: " + str(time.time() - tinit), Print.ORANGE)
         return next_target
-
