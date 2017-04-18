51428444d4231efec87d1d45fd144290440a946e
orestisf <orestisf1993@gmail.com>
2017-04-18 20:14:01 +0300

    Reformat utilities.py

--- art_autonomous_exploration/src/utilities.py
+++ art_autonomous_exploration/src/utilities.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 
-import rospy
-import random
-import math
 import time
+
 import numpy as np
-from timeit import default_timer as timer
+import rospy
+from _cpp_functions import ffi, lib
 from visualization_msgs.msg import Marker
 from visualization_msgs.msg import MarkerArray
 
-from _cpp_functions import ffi, lib
 
-class Print:
+class Print(object):
+    def __init__(self):
+        pass
+
     HEADER = '\033[95m'
     BLUE = '\033[94m'
     GREEN = '\033[92m'
@@ -23,190 +24,188 @@ class Print:
 
     @staticmethod
     def art_print(txt, color):
-      print color + str(txt) + Print.END
+        print color + str(txt) + Print.END
 
-class Cffi:
+
+class Cffi(object):
+    def __init__(self):
+        pass
+
     @staticmethod
     def brushfireFromObstacles(ogm, brush, ogml):
-      #TODO improve: by map limits
-      x = [np.array(v, dtype='int32') for v in ogm]
-      xi = ffi.new(("int* [%d]") % (len(x)))
-      for i in range(len(x)):
-        xi[i] = ffi.cast("int *", x[i].ctypes.data)
+        # TODO improve: by map limits
+        x = [np.array(v, dtype='int32') for v in ogm]
+        xi = ffi.new("int* [%d]" % (len(x)))
+        for i in range(len(x)):
+            xi[i] = ffi.cast("int *", x[i].ctypes.data)
 
-      y = [np.array(v, dtype='int32') for v in brush]
-      yi = ffi.new(("int* [%d]") % (len(y)))
-      for i in range(len(y)):
-        yi[i] = ffi.cast("int *", y[i].ctypes.data)
+        y = [np.array(v, dtype='int32') for v in brush]
+        yi = ffi.new("int* [%d]" % (len(y)))
+        for i in range(len(y)):
+            yi[i] = ffi.cast("int *", y[i].ctypes.data)
 
-      br_c = lib.brushfireFromObstacles(xi, yi, len(x), len(x[0]), 
-          ogml['min_x'], ogml['max_x'], ogml['min_y'], ogml['max_y'])
-      # TODO: Must be faster!
-      for i in range(ogm.shape[0]):
-        for j in range(ogm.shape[1]):
-          brush[i][j] = yi[i][j]
-
-      return brush
+        # noinspection PyUnusedLocal
+        br_c = lib.brushfireFromObstacles(xi, yi, len(x), len(x[0]), ogml['min_x'], ogml['max_x'], ogml['min_y'],
+                                          ogml['max_y'])
+        # TODO: Must be faster!
+        for i in range(ogm.shape[0]):
+            for j in range(ogm.shape[1]):
+                brush[i][j] = yi[i][j]
+        return brush
 
     @staticmethod
     def thinning(skeleton, ogml):
-      x = [np.array(v, dtype='int32') for v in skeleton]
-      xi = ffi.new(("int* [%d]") % (len(x)))
-      for i in range(len(x)):
-        xi[i] = ffi.cast("int *", x[i].ctypes.data)
+        x = [np.array(v, dtype='int32') for v in skeleton]
+        xi = ffi.new("int* [%d]" % (len(x)))
+        for i in range(len(x)):
+            xi[i] = ffi.cast("int *", x[i].ctypes.data)
 
-      y = [np.array(v, dtype='int32') for v in skeleton]
-      yi = ffi.new(("int* [%d]") % (len(y)))
-      for i in range(len(y)):
-        yi[i] = ffi.cast("int *", y[i].ctypes.data)
+        y = [np.array(v, dtype='int32') for v in skeleton]
+        yi = ffi.new("int* [%d]" % (len(y)))
+        for i in range(len(y)):
+            yi[i] = ffi.cast("int *", y[i].ctypes.data)
 
-      itime = time.time()
-      br_c = lib.thinning(xi, yi, len(x), len(x[0]),
-          ogml['min_x'], ogml['max_x'], ogml['min_y'], ogml['max_y'])
-      Print.art_print("Pure skeletonization time: " + str(time.time() - itime), Print.BLUE)
-      # TODO: Must be faster!
-      itime = time.time()
-      for i in range(skeleton.shape[0]):
-        for j in range(skeleton.shape[1]):
-          skeleton[i][j] = yi[i][j]
-      Print.art_print("Skeletonization final copy: " + str(time.time() - itime), Print.BLUE)
-      return skeleton
+        itime = time.time()
+        # noinspection PyUnusedLocal
+        br_c = lib.thinning(xi, yi, len(x), len(x[0]), ogml['min_x'], ogml['max_x'], ogml['min_y'], ogml['max_y'])
+        Print.art_print("Pure skeletonization time: " + str(time.time() - itime), Print.BLUE)
+
+        # TODO: Must be faster!
+        itime = time.time()
+        for i in range(skeleton.shape[0]):
+            for j in range(skeleton.shape[1]):
+                skeleton[i][j] = yi[i][j]
+        Print.art_print("Skeletonization final copy: " + str(time.time() - itime), Print.BLUE)
+        return skeleton
 
     @staticmethod
     def prune(skeleton, ogml, iterations):
-      itime = time.time()
-      x = [np.array(v, dtype='int32') for v in skeleton]
-      xi = ffi.new(("int* [%d]") % (len(x)))
-      for i in range(len(x)):
-        xi[i] = ffi.cast("int *", x[i].ctypes.data)
+        itime = time.time()
+        x = [np.array(v, dtype='int32') for v in skeleton]
+        xi = ffi.new("int* [%d]" % (len(x)))
+        for i in range(len(x)):
+            xi[i] = ffi.cast("int *", x[i].ctypes.data)
 
-      y = [np.array(v, dtype='int32') for v in skeleton]
-      yi = ffi.new(("int* [%d]") % (len(y)))
-      for i in range(len(y)):
-        yi[i] = ffi.cast("int *", y[i].ctypes.data)
+        y = [np.array(v, dtype='int32') for v in skeleton]
+        yi = ffi.new("int* [%d]" % (len(y)))
+        for i in range(len(y)):
+            yi[i] = ffi.cast("int *", y[i].ctypes.data)
 
-      br_c = lib.prune(xi, yi, len(x), len(x[0]),
-          ogml['min_x'], ogml['max_x'], ogml['min_y'], ogml['max_y'], iterations)
-      
-      # TODO: Must be faster!
-      for i in range(skeleton.shape[0]):
-        for j in range(skeleton.shape[1]):
-          skeleton[i][j] = yi[i][j]
-      Print.art_print("Pruning time: " + str(time.time() - itime), Print.BLUE)
-      return skeleton
+        # noinspection PyUnusedLocal
+        br_c = lib.prune(xi, yi, len(x), len(x[0]), ogml['min_x'], ogml['max_x'], ogml['min_y'], ogml['max_y'],
+                         iterations)
 
-class OgmOperations:
+        # TODO: Must be faster!
+        for i in range(skeleton.shape[0]):
+            for j in range(skeleton.shape[1]):
+                skeleton[i][j] = yi[i][j]
+        Print.art_print("Pruning time: " + str(time.time() - itime), Print.BLUE)
+        return skeleton
+
+
+class OgmOperations(object):
+    def __init__(self):
+        pass
 
     @staticmethod
     def blurUnoccupiedOgm(ogm, ogml):
-      local = np.copy(ogm)
-      for i in range(ogml['min_x'], ogml['max_x']):
-        for j in range(ogml['min_y'], ogml['max_y']):
-          if ogm[i][j] > 49: # Not free
-            c = 0
-            for ii in range(-1, 2):
-              for jj in range(-1, 2):
-                if ogm[i + ii][j + jj] <= 49:
-                  c += 1
-            if c >= 4:
-              local[i][j] = 0
-      return local
+        local = np.copy(ogm)
+        for i in range(ogml['min_x'], ogml['max_x']):
+            for j in range(ogml['min_y'], ogml['max_y']):
+                if ogm[i][j] > 49:  # Not free
+                    c = 0
+                    for ii in range(-1, 2):
+                        for jj in range(-1, 2):
+                            if ogm[i + ii][j + jj] <= 49:
+                                c += 1
+                    if c >= 4:
+                        local[i][j] = 0
+        return local
 
     @staticmethod
     def findUsefulBoundaries(ogm, origin, resolution):
-      min_x = origin['x'] / resolution
-      min_y = origin['y'] / resolution
-      max_x = origin['x'] / resolution
-      max_y = origin['y'] / resolution
-      
-      x = ogm.shape[0]
-      y = ogm.shape[1]
+        min_x = origin['x'] / resolution
+        min_y = origin['y'] / resolution
+        max_x = origin['x'] / resolution
+        max_y = origin['y'] / resolution
 
-      # Search by x min
-      ok = False
-      for i in range(0, x, 20):
-        for j in range(0, y):
-          if ogm[i][j] != 51:
-            min_x = i - 20
-            ok = True
-            break
-        if ok:
-          break
+        x = ogm.shape[0]
+        y = ogm.shape[1]
 
-      # Search by x max
-      ok = False
-      for i in range(min_x + 20, x, 20):
-        for j in range(0, y):
-          if ogm[i][j] != 51:
-            ok = True
-            break
-        if not ok:
-          max_x = i
-          break
+        # Search by x min
         ok = False
+        for i in range(0, x, 20):
+            for j in range(0, y):
+                if ogm[i][j] != 51:
+                    min_x = i - 20
+                    ok = True
+                    break
+            if ok:
+                break
 
-      # Search by y min
-      ok = False
-      for j in range(0, y, 20):
-        for i in range(0, x):
-          if ogm[i][j] != 51:
-            min_y = j - 20
-            ok = True
-            break
-        if ok:
-          break
-
-      # Search by y max
-      ok = False
-      for j in range(min_y + 20, y, 20):
-        for i in range(0, x):
-          if ogm[i][j] != 51:
-            ok = True
-            break
-        if not ok:
-          max_y = j
-          break
+        # Search by x max
         ok = False
+        for i in range(min_x + 20, x, 20):
+            for j in range(0, y):
+                if ogm[i][j] != 51:
+                    ok = True
+                    break
+            if not ok:
+                max_x = i
+                break
+            ok = False
 
-      RvizHandler.printMarker(\
-            [
-                [
-                    min_x * resolution + origin['x'],
-                    min_y * resolution + origin['y']
-                ],
-                [
-                    max_x * resolution + origin['x'], 
-                    min_y * resolution + origin['y']
-                ],
-                [
-                    max_x * resolution + origin['x'], 
-                    max_y * resolution + origin['y']
-                ],
-                [
-                    min_x * resolution + origin['x'], 
-                    max_y * resolution + origin['y']
-                ]
-            ],\
-            1, # Type: Line strip
-            0, # Action: Add
-            "map", # Frame
-            "art_ogm_boundary", # Namespace
-            [0, 0.9, 0, 1.0], # Color RGBA
-            0.2 # Scale
+        # Search by y min
+        ok = False
+        for j in range(0, y, 20):
+            for i in range(0, x):
+                if ogm[i][j] != 51:
+                    min_y = j - 20
+                    ok = True
+                    break
+            if ok:
+                break
+
+        # Search by y max
+        ok = False
+        for j in range(min_y + 20, y, 20):
+            for i in range(0, x):
+                if ogm[i][j] != 51:
+                    ok = True
+                    break
+            if not ok:
+                max_y = j
+                break
+            ok = False
+
+        RvizHandler.printMarker(
+            [[min_x * resolution + origin['x'], min_y * resolution + origin['y']],
+             [max_x * resolution + origin['x'], min_y * resolution + origin['y']],
+             [max_x * resolution + origin['x'], max_y * resolution + origin['y']],
+             [min_x * resolution + origin['x'], max_y * resolution + origin['y']]
+             ],
+            1,  # Type: Line strip
+            0,  # Action: Add
+            "map",  # Frame
+            "art_ogm_boundary",  # Namespace
+            [0, 0.9, 0, 1.0],  # Color RGBA
+            0.2  # Scale
         )
 
+        return {
+            'min_x': min_x,
+            'max_x': max_x,
+            'min_y': min_y,
+            'max_y': max_y
+        }
 
-      return {
-          'min_x': min_x, 
-          'max_x': max_x, 
-          'min_y': min_y, 
-          'max_y': max_y
-          }
 
-class RvizHandler:
+class RvizHandler(object):
+    def __init__(self):
+        pass
 
     markers_publisher = \
-            rospy.Publisher('art_rviz_markers', MarkerArray, queue_size = 10)
+        rospy.Publisher('art_rviz_markers', MarkerArray, queue_size=10)
 
     # Poses: list of coordinates
     # m_type: http://wiki.ros.org/rviz/DisplayTypes/Marker
@@ -222,7 +221,6 @@ class RvizHandler:
         markers = MarkerArray()
         c = 0
         for s in poses:
-            #print "Print " + str(s) + " for ns = " + ns
             c += 1
             st = Marker()
             st.header.frame_id = frame
@@ -246,30 +244,28 @@ class RvizHandler:
             markers.markers.append(st)
 
             if ns == 'art_topological_nodes':
-              c += 1
-              st = Marker()
-              st.header.frame_id = frame
-              st.ns = ns
-              st.id = c
-              st.header.stamp = rospy.Time(0)
-              st.type = 9
-              st.text = str((int) (c/2 - 1))
-              st.action = action
+                c += 1
+                st = Marker()
+                st.header.frame_id = frame
+                st.ns = ns
+                st.id = c
+                st.header.stamp = rospy.Time(0)
+                st.type = 9
+                st.text = str(int(c / 2 - 1))
+                st.action = action
 
-              st.pose.position.x = s[0] + 0.15
-              st.pose.position.y = s[1] + 0.15
-              st.pose.position.z = 0.2
+                st.pose.position.x = s[0] + 0.15
+                st.pose.position.y = s[1] + 0.15
+                st.pose.position.z = 0.2
 
-              st.color.r = 0
-              st.color.g = 0
-              st.color.b = 0
-              st.color.a = 0.8
+                st.color.r = 0
+                st.color.g = 0
+                st.color.b = 0
+                st.color.a = 0.8
 
-              st.scale.x = scale * 4
-              st.scale.y = scale * 4
-              st.scale.z = scale * 4
-              markers.markers.append(st)
-
+                st.scale.x = scale * 4
+                st.scale.y = scale * 4
+                st.scale.z = scale * 4
+                markers.markers.append(st)
 
         RvizHandler.markers_publisher.publish(markers)
-
