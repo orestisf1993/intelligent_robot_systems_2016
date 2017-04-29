0a01ea4292cf2199eeb4a19a27a6b4a717f19294
orestisf <orestisf1993@gmail.com>
2017-04-30 02:30:29 +0300

    squash! WIP: cost based
    Improve topo cost logic

--- art_autonomous_exploration/src/target_selection.py
+++ art_autonomous_exploration/src/target_selection.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from __future__ import division
 
+import itertools
 import math
 import random
 import time
@@ -124,13 +125,13 @@ class TargetSelection(object):
 
     def select_by_cost(self, map_info):
         numpy.set_printoptions(precision=3, threshold=numpy.nan, suppress=True)  # TODO:del
-        nodes, paths = self.choose_best_nodes(map_info)
+        nodes, paths, topo_costs = zip(*self.choose_best_nodes(map_info))
         if not nodes:
             return -1, -1
 
         best_path_idx = self.weight_costs(
             # TODO: one for path in paths?
-            [self.topo_cost_path(path, map_info.ogm) for path in paths],  # Topological
+            topo_costs,
             [self.coverage_cost(path, map_info.coverage) for path in paths],  # Coverage
             [self.distance_cost(path, map_info.robot_px) for path in paths],  # Distance
             [self.rotation_cost(path, map_info.robot_px, map_info.theta) for path in paths]  # Rotational
@@ -144,44 +145,38 @@ class TargetSelection(object):
     def choose_best_nodes(self, map_info):
         # Since path planning takes a lot of time for many nodes we should reduce the possible result to the nodes
         # with the best distance and topological costs.
-        def distance_cost_from_robot(point):
-            robot_px = map_info.robot_px
-            return self.distance_coeff(point, robot_px) * self.distance(point, robot_px)
 
+        topo_costs = [self._topological_cost(node, map_info.ogm) for node in nodes]
         best_nodes_idx = self.weight_costs(
-            [self._topological_cost(node, map_info.ogm) for node in map_info.nodes],
-            [distance_cost_from_robot(node) for node in map_info.nodes]
+            topo_costs,
+            [euclidean(node, map_info.robot_px) for node in nodes]
         ).argsort()[::-1]  # We need descending order since we now want to maximize.
 
         count = 0
-        paths = []
-        nodes_new = []
         for idx in best_nodes_idx:
-            node = map_info.nodes[idx]
+            node = nodes[idx]
             path = map_info.create_path(node)
             if path:
                 count += 1
-                paths.append(path)
-                nodes_new.append(node)
-            if count == 5:  # TODO:configurable
+                yield node, path, topo_costs[idx]
+            if count == 7:  # TODO:configurable
                 break
-        return nodes_new, paths
 
     def weight_costs(self, *cost_vectors, **kwargs):
         costs = self.normalize_costs(numpy.array(tuple(vector for vector in cost_vectors)))
-        Print.art_print(str(costs), Print.BLUE)  # TODO:del
+        Print.art_print("After normalize:\n" + str(costs), Print.BLUE)  # TODO:del
         if 'weights' in kwargs:
             weights = kwargs['weights']
         else:
             weights = 2 ** numpy.arange(costs.shape[0] - 1, -1, -1)
         return numpy.average(costs, axis=0, weights=weights)
 
-    def topo_cost_path(self, path, ogm):
-        return sum(self.topo_cost(node, ogm) for node in path)
-
-    # TODO: topo -> topological in file
-    def topo_cost(self, node, ogm):
+    def _topological_cost(self, node, ogm):
         threshold = self.cost_based_properties['topo_threshold']
+        return self.topological_cost(node, ogm, threshold)
+
+    @staticmethod
+    def topological_cost(node, ogm, threshold):
         result = 0
         for dir_x, dir_y in [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]:
             cost = 0
@@ -194,7 +189,11 @@ class TargetSelection(object):
                 cost = (x - x_c) ** 2 + (y - y_c) ** 2
                 if ogm[x_c][y_c] > 50:
                     break
-            result += 1 / min(threshold, cost)
+                elif ogm[x_c][y_c] in [50, -1]:
+                    Print.art_print("{},{} is unknown!".format(x_c, y_c), Print.RED)
+                    cost = threshold
+                    break
+            result += min(threshold, cost)
         return result
 
     @staticmethod
