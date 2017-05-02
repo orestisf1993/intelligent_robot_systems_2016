549b260c77568edf8cf0e25cb932588607eefa79
orestisf <orestisf1993@gmail.com>
2017-05-02 19:11:47 +0300

    Try to prevent creating paths that pass through obstacles

--- art_ogmpp/ogmpp_planners/src/ogmpp_planners/ogmpp_prms/ogmpp_uniform_sampling.cpp
+++ art_ogmpp/ogmpp_planners/src/ogmpp_planners/ogmpp_prms/ogmpp_uniform_sampling.cpp
@@ -54,14 +54,56 @@ namespace ogmpp_planners {
                         map.getDistanceTransformation(x, y) > min_dist_from_wall) {
                         _g.addNode(ogmpp_graph::Cell(x, y));
                         // Make connections
-                        if (x - step >= 0 && y - step >= 0 && _g.getNode(ogmpp_graph::Cell(x - step, y - step)) != NULL)
-                            _g.makeNeighbor(ogmpp_graph::Cell(x, y), ogmpp_graph::Cell(x - step, y - step));
-                        if (y - step >= 0 && _g.getNode(ogmpp_graph::Cell(x, y - step)) != NULL)
-                            _g.makeNeighbor(ogmpp_graph::Cell(x, y), ogmpp_graph::Cell(x, y - step));
-                        if (x - step >= 0 && y + step < h && _g.getNode(ogmpp_graph::Cell(x - step, y + step)) != NULL)
-                            _g.makeNeighbor(ogmpp_graph::Cell(x, y), ogmpp_graph::Cell(x - step, y + step));
-                        if (x - step >= 0 && _g.getNode(ogmpp_graph::Cell(x - step, y)) != NULL)
-                            _g.makeNeighbor(ogmpp_graph::Cell(x, y), ogmpp_graph::Cell(x - step, y));
+                        if (x - step >= 0 && y - step >= 0 &&
+                            _g.getNode(ogmpp_graph::Cell(x - step, y - step)) != NULL) {
+                            bool throughWall = false;
+                            for (int i = 1; i < step && !throughWall; i++) {
+                                if (!map.isUnoccupied(x - i, y - i)) {
+                                    throughWall = true;
+                                    ROS_WARN_STREAM("Discarding wall neighbor!");
+                                }
+                            }
+                            if (!throughWall) {
+                                _g.makeNeighbor(ogmpp_graph::Cell(x, y), ogmpp_graph::Cell(x - step, y - step));
+                            }
+                        }
+                        if (y - step >= 0 && _g.getNode(ogmpp_graph::Cell(x, y - step)) != NULL) {
+                            bool throughWall = false;
+                            for (int i = 1; i < step && !throughWall; i++) {
+                                if (!map.isUnoccupied(x, y - i)) {
+                                    throughWall = true;
+                                    ROS_WARN_STREAM("Discarding wall neighbor!");
+                                }
+                            }
+                            if (!throughWall) {
+                                _g.makeNeighbor(ogmpp_graph::Cell(x, y), ogmpp_graph::Cell(x, y - step));
+                            }
+                        }
+                        if (x - step >= 0 && y + step < h &&
+                            _g.getNode(ogmpp_graph::Cell(x - step, y + step)) != NULL) {
+                            bool throughWall = false;
+                            for (int i = 1; i < step && !throughWall; i++) {
+                                if (!map.isUnoccupied(x - i, y + i)) {
+                                    throughWall = true;
+                                    ROS_WARN_STREAM("Discarding wall neighbor!");
+                                }
+                            }
+                            if (!throughWall) {
+                                _g.makeNeighbor(ogmpp_graph::Cell(x, y), ogmpp_graph::Cell(x - step, y + step));
+                            }
+                        }
+                        if (x - step >= 0 && _g.getNode(ogmpp_graph::Cell(x - step, y)) != NULL) {
+                            bool throughWall = false;
+                            for (int i = 1; i < step && !throughWall; i++) {
+                                if (!map.isUnoccupied(x - i, y)) {
+                                    throughWall = true;
+                                    ROS_WARN_STREAM("Discarding wall neighbor!");
+                                }
+                            }
+                            if (!throughWall) {
+                                _g.makeNeighbor(ogmpp_graph::Cell(x, y), ogmpp_graph::Cell(x - step, y));
+                            }
+                        }
                     }
                     y += step;
                 }
@@ -72,8 +114,6 @@ namespace ogmpp_planners {
             if (!map.isUnoccupied(begin.x, begin.y) || !map.isUnoccupied(end.x, end.y) ||
                 map.isUnknown(begin.x, begin.y) || map.isUnknown(end.x, end.y)) {
                 ROS_ERROR_STREAM("ogmpp_uniform_sampling: Robot or goal pose is not unoccupied");
-                _g.clean();
-                return _g;
             }
 
             _g.addNode(begin);
